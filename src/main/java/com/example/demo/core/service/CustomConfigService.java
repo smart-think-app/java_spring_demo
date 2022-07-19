4e5e5ba5694f46fde650165013ab99d5d3d1c886
smart-think-app <79594145+smart-think-app@users.noreply.github.com>
2022-07-19 20:35:32 +0700

    code

---
+++ src/main/java/com/example/demo/core/service/CustomConfigService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.core.service;
+
+import com.example.demo.core.inter.CustomConfigInterface;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author DELL
+ */
+@Component
+public class CustomConfigService implements CustomConfigInterface{
+    
+}
