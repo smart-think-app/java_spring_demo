4c4415f37fdbf9a1bbe7952f8b407fd3ebef4ecb
smart-think-app <79594145+smart-think-app@users.noreply.github.com>
2022-07-19 20:33:58 +0700

    code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,17 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner{
+public class DemoApplication{
 
 	public static void main(String[] args) {
                 SpringApplication app = new SpringApplication(DemoApplication.class);
                 app.setDefaultProperties(Collections.singletonMap("server.port", "3000"));
                 app.run(args);
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 
 }
